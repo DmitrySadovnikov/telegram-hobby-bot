bb0fb783d4259c7fb29ed1a0da7dc83d12f13311
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-05-28 22:50:02 +0300

    123

--- app/servers/hobby/message_server.rb
+++ app/servers/hobby/message_server.rb
@@ -4,9 +4,11 @@ class Hobby::MessageServer
   def call(text:, chat_id:)
     text =
       case text.downcase
-      when /привет/i then 'Ну привет'
-      when 'all'     then  Hobby.all.pluck(:title).join("\n")
-      else                 Hobby.all.pluck(:title).sample
+      when /привет/i  then  'Ну привет'
+      when /котлета/i then  'Это будет жаркое лето'
+      when /букашка/i then  'Как дела Машка?'
+      when 'all'      then  Hobby.all.pluck(:title).join("\n")
+      else                  Hobby.all.pluck(:title).sample
       end
 
     post chat_id: chat_id, text: text
